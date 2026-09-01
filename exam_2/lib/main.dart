619c1d1c6b32947ba4468ee24572f8b0a6c97d58
invalid-code <jessgaming55@gmail.com>
2026-09-01 16:14:14 +0800

    feat: updated the ui

--- exam_2/lib/main.dart
+++ exam_2/lib/main.dart
@@ -28,15 +28,15 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.dark(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -47,28 +47,93 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    List<int> valorantFriends = [0, 1, 2];
+    List<String> friends = [
+      "MissYouLikeKrazy",
+      "bread",
+      "The14th",
+      "Carlvendish",
+      "D1yah",
+    ];
+    Card socialCard = Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 2.0,
+      child: const Text("Social"),
+    );
+    Card tabsCard = Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 2.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Friends"),
+          const Text("Messages"),
+          const Text("Requests"),
+        ],
+      ),
+    );
+    Card searchBarCard = Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 2.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [const Text("Search")],
+      ),
+    );
+    Card gameCard = Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 2.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [const Text("Valorant"), Text("${valorantFriends.length}")],
+      ),
+    );
+    Card gameFriendsCard = valorantFriends.isNotEmpty
+        ? Card(
+            clipBehavior: Clip.antiAlias,
+            elevation: 2.0,
+            child: ListView.builder(
+              itemCount: valorantFriends.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  // children: [
+                  //   Text(friends[valorantFriends[index]]),
+                  // ],
+                  title: Text(friends[valorantFriends[index]]),
+                );
+              },
+            ),
+          )
+        : Card(
+            clipBehavior: Clip.antiAlias,
+            elevation: 2.0,
+            child: SizedBox(height: 0),
+          );
+    Card onlineCard = Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 2.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [const Text("Online"), Text("${friends.length}")],
+      ),
+    );
+    Card onlineFriendsCard = friends.isNotEmpty
+        ? Card(
+            clipBehavior: Clip.antiAlias,
+            elevation: 2.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [],
+            ),
+          )
+        : Card(
+            clipBehavior: Clip.antiAlias,
+            elevation: 2.0,
+            child: SizedBox(height: 0),
+          );
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -83,39 +148,38 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Hello World"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        //
+        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        // action in the IDE, or press "p" in the console), to see the
+        // wireframe for each widget.
+        mainAxisAlignment: .center,
+        children: [
+          socialCard,
+          const SizedBox(height: 5),
+          tabsCard,
+          const SizedBox(height: 5),
+          searchBarCard,
+          const SizedBox(height: 5),
+          gameCard,
+          const SizedBox(height: 5),
+          gameFriendsCard,
+          const SizedBox(height: 5),
+          onlineCard,
+          const SizedBox(height: 5),
+          onlineFriendsCard,
+        ],
       ),
     );
   }
